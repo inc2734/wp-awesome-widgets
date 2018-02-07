9c025c8cf5a44b332a1ea1ffd3703d9160cee079
Takashi Kitajima <inc@2inc.org>
2018-02-08 01:46:21 +0900

    Fix color picker bug

--- src/assets/js/color-picker-field.js
+++ src/assets/js/color-picker-field.js
@@ -3,6 +3,17 @@ jQuery(function($) {
     _initColorPicker(widget);
   });
 
+  $(document).on('wpaw-repeaters-add-repeater', function (event, data) {
+    var repeater  = data.repeater;
+    repeater.find('.wpaw-color-picker-field').each(function(i, e) {
+      var input       = $(e).find('.wpaw-color-picker-field__input').clone(false);
+      var colorPicker = $(e).find('.wp-picker-container');
+      colorPicker.before(input);
+      colorPicker.remove();
+    });
+    _initColorPicker(repeater);
+  });
+
   $(document).ready(function() {
     $('#widgets-right .widget:has(.wpaw-color-picker-field__input) .widget-inside').each(function(i, e) {
       _initColorPicker($(e));
@@ -10,10 +21,13 @@ jQuery(function($) {
   });
 
   function _initColorPicker(widget) {
-    widget.find('.wpaw-color-picker-field__input').wpColorPicker( {
-      change: _.throttle(function() {
-        $(this).trigger('change');
-      }, 3000)
+    widget.find('.wpaw-color-picker-field__input').each(function(i, e) {
+      var input = $(e);
+      input.wpColorPicker({
+        change: function() {
+          input.trigger('input');
+        }
+      });
     });
   }
 });
