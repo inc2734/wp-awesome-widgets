77e8b5d2669881512c0b62a3201e5e8411303ab3
Takashi Kitajima <inc@2inc.org>
2019-03-05 00:50:40 +0900

    Update test suites

--- tests/test-sample.php
+++ tests/test-sample.php
@@ -14,6 +14,8 @@ class Sample_Test extends WP_UnitTestCase {
 	 * @test
 	 */
 	public function sample() {
-		new Inc2734\WP_Awesome_Widgets\Bootstrap();
+		$bootstrap = new Inc2734\WP_Awesome_Widgets\Bootstrap();
+
+		$this->assertTrue( is_a( $bootstrap, 'Inc2734\WP_Awesome_Widgets\Bootstrap' ) );
 	}
 }
