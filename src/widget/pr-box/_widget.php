4cba1bdd3c2fe6b866df20ac2711190cc926aa17
Takashi Kitajima <inc@2inc.org>
2019-06-27 10:33:13 +0900

    https://github.com/inc2734/snow-monkey/issues/575

--- src/widget/pr-box/_widget.php
+++ src/widget/pr-box/_widget.php
@@ -37,47 +37,49 @@
 				</div>
 			<?php endif; ?>
 
-			<div class="wpaw-pr-box__row wpaw-pr-box__row--<?php echo esc_attr( $instance['sm-columns'] ); ?> wpaw-pr-box__row--md-<?php echo esc_attr( $instance['md-columns'] ); ?>  wpaw-pr-box__row--lg-<?php echo esc_attr( $instance['lg-columns'] ); ?>">
-				<?php foreach ( $instance['items'] as $item ) : ?>
-					<div class="wpaw-pr-box__item">
-						<?php if ( ! empty( $item['src'] ) ) : ?>
-							<?php
-							$aspect_ratio   = $instance['thumbnail-aspect-ratio'];
-							$thumbnail_size = apply_filters( 'inc2734_wp_awesome_widgets_pr_box_thumbnail_size', 'xlarge', wp_is_mobile(), $args['widget_id'] );
-							?>
-							<?php if ( ! empty( $item['link-url'] ) ) : ?>
-								<a href="<?php echo esc_html( $item['link-url'] ); ?>">
+			<?php if ( ! empty( $instance['items'] ) ) : ?>
+				<div class="wpaw-pr-box__row wpaw-pr-box__row--<?php echo esc_attr( $instance['sm-columns'] ); ?> wpaw-pr-box__row--md-<?php echo esc_attr( $instance['md-columns'] ); ?>  wpaw-pr-box__row--lg-<?php echo esc_attr( $instance['lg-columns'] ); ?>">
+					<?php foreach ( $instance['items'] as $item ) : ?>
+						<div class="wpaw-pr-box__item">
+							<?php if ( ! empty( $item['src'] ) ) : ?>
+								<?php
+								$aspect_ratio   = $instance['thumbnail-aspect-ratio'];
+								$thumbnail_size = apply_filters( 'inc2734_wp_awesome_widgets_pr_box_thumbnail_size', 'xlarge', wp_is_mobile(), $args['widget_id'] );
+								?>
+								<?php if ( ! empty( $item['link-url'] ) ) : ?>
+									<a href="<?php echo esc_html( $item['link-url'] ); ?>">
+										<div class="wpaw-pr-box__item-figure wpaw-pr-box__item-figure--<?php echo esc_attr( $aspect_ratio ); ?>">
+											<?php echo wp_get_attachment_image( $item['src'], $thumbnail_size ); ?>
+										</div>
+									</a>
+								<?php else : ?>
 									<div class="wpaw-pr-box__item-figure wpaw-pr-box__item-figure--<?php echo esc_attr( $aspect_ratio ); ?>">
 										<?php echo wp_get_attachment_image( $item['src'], $thumbnail_size ); ?>
 									</div>
-								</a>
-							<?php else : ?>
-								<div class="wpaw-pr-box__item-figure wpaw-pr-box__item-figure--<?php echo esc_attr( $aspect_ratio ); ?>">
-									<?php echo wp_get_attachment_image( $item['src'], $thumbnail_size ); ?>
+								<?php endif; ?>
+							<?php endif; ?>
+
+							<?php if ( ! empty( $item['title'] ) ) : ?>
+								<div class="wpaw-pr-box__item-title"><?php echo wp_kses_post( $item['title'] ); ?></div>
+							<?php endif; ?>
+
+							<?php if ( ! empty( $item['summary'] ) ) : ?>
+								<div class="wpaw-pr-box__item-summary">
+									<?php echo wp_kses_post( wpautop( $item['summary'] ) ); ?>
 								</div>
 							<?php endif; ?>
-						<?php endif; ?>
 
-						<?php if ( ! empty( $item['title'] ) ) : ?>
-							<div class="wpaw-pr-box__item-title"><?php echo wp_kses_post( $item['title'] ); ?></div>
-						<?php endif; ?>
-
-						<?php if ( ! empty( $item['summary'] ) ) : ?>
-							<div class="wpaw-pr-box__item-summary">
-								<?php echo wp_kses_post( wpautop( $item['summary'] ) ); ?>
-							</div>
-						<?php endif; ?>
-
-						<?php if ( ! empty( $item['link-url'] ) && ! empty( $item['link-text'] ) ) : ?>
-							<div class="wpaw-pr-box__item-action">
-								<a class="wpaw-pr-box__item-more" href="<?php echo esc_url( $item['link-url'] ); ?>">
-									<?php echo esc_html( $item['link-text'] ); ?>
-								</a>
-							</div>
-						<?php endif; ?>
-					</div>
-				<?php endforeach; ?>
-			</div>
+							<?php if ( ! empty( $item['link-url'] ) && ! empty( $item['link-text'] ) ) : ?>
+								<div class="wpaw-pr-box__item-action">
+									<a class="wpaw-pr-box__item-more" href="<?php echo esc_url( $item['link-url'] ); ?>">
+										<?php echo esc_html( $item['link-text'] ); ?>
+									</a>
+								</div>
+							<?php endif; ?>
+						</div>
+					<?php endforeach; ?>
+				</div>
+			<?php endif; ?>
 
 			<?php if ( ! empty( $instance['link-url'] ) && ! empty( $instance['link-text'] ) ) : ?>
 				<div class="wpaw-pr-box__action">
