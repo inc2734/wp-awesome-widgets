ac5dd9ff0d392a96513fed5a3a96bf4c39669bb4
Takashi Kitajima <inc@2inc.org>
2020-07-06 10:21:55 +0900

    https://github.com/inc2734/snow-monkey/issues/839

--- src/templates/google-adsense.php
+++ src/templates/google-adsense.php
@@ -11,7 +11,7 @@ use Inc2734\WP_Awesome_Widgets\Helper;
 <?php echo wp_kses_post( $args['before_widget'] ); ?>
 
 	<div
-		class="wpaw-google-adsense wpaw-google-adsense--<?php echo esc_attr( $args['widget_id'] ); ?>"
+		class="wpaw-google-adsense wpaw-google-adsense--<?php echo esc_attr( $args['widget_id'] ); ?> wpaw-google-adsense--<?php echo esc_attr( $instance['size'] ); ?>"
 		id="wpaw-google-adsense-<?php echo esc_attr( $args['widget_id'] ); ?>"
 		>
 
