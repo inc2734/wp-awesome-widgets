ecd614f7509c444f5b024a6376af9d127741746e
inc2734 <inc@2inc.org>
2020-10-21 10:57:38 +0900

    https://github.com/inc2734/snow-monkey/issues/934

--- src/App/View.php
+++ src/App/View.php
@@ -11,6 +11,14 @@ use Inc2734\WP_Plugin_View_Controller\Bootstrap;
 
 class View {
 	public static function render( $slug, $name, $widget_args ) {
+		$widget_args = wp_parse_args(
+			$widget_args,
+			[
+				'widget_args' => [],
+				'instance'    => [],
+			]
+		);
+
 		$bootstrap = new Bootstrap(
 			[
 				'prefix' => 'inc2734_wp_awesome_widgets_',
